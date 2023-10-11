a9be459937994868859437a94fd622e031013d04
Kuba-Bigaj <k.bigaj666@gmail.com>
2023-10-11 22:14:04 +0200

    Framework basis

--- PEA1/PEA1.cpp
+++ PEA1/PEA1.cpp
@@ -2,10 +2,81 @@
 //
 
 #include <iostream>
+#include <string>
+#include <conio.h>
+
+
+class App {
+	//menu elements
+	std::string title = "MENU GŁÓWNE";
+	std::string credits = "Kuba Bigaj 2023";
+	std::string[3] options = { "ALgorytmy", "Dane", "Wyjście" };
+
+	//data
+	int** matrix=nullptr;
+
+	void load_random_data(int size)
+	{
+		if (!matrix)
+		{
+			delete matrix;
+		}
+
+		matrix = (int**) new int[size*size];
+
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				matrix[i][j] = rand() % 100 + 1;
+			}
+		}
+	}
+
+	static int create_sub_menu(std::string top_banner, std::string options[], std::string bot_banner, int number, int def_option) // displays a submenu with options, returns the number chosen
+	{
+		int chosen_option = def_option;
+		while (true)
+		{
+			system("cls");
+			std::cout << top_banner;
+			for (int i = 0; i < number; i++)
+			{
+				if (i == chosen_option)
+				{
+					std::cout << "==>";
+				}
+				std::cout << "\t" << options[i] << "\n";
+			}
+			switch (getch())
+			{
+			case 72:
+				chosen_option--;
+				break;
+			case 80:
+				chosen_option++;
+				break;
+			case '\r':
+				return chosen_option;
+			default:
+				break;
+			}
+			chosen_option = (chosen_option < 0 ? 0 : (chosen_option >= number ? number - 1 : chosen_option));
+		}
+	}
+
+public:
+	void run()
+	{
+		create_sub_menu(title, options, credits, 3, 0);
+	}
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	std::cout << "Hello World!\n";
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
